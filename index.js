b1d9b3e455a2c0fb021a99c762a0a29e428bf8d2
Nguyen Ngoc Khanh <nnkhanh.20it12@vku.udn.vn>
2021-11-24 23:04:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/connectDB");
 const initRouter = require("./router/index");
 
+const origin =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://social-network-app-fake.netlify.app/";
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 connectDB();
